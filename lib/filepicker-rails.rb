e9442d3611782d4d18b8d7207cc98777f0043af2
Vinh Bui <vinh@holdenbrand.com>
2025-01-26 17:50:43 -0600

    Fix autoload errors for Rails 6.x

--- lib/filepicker-rails.rb
+++ lib/filepicker-rails.rb
@@ -2,6 +2,8 @@ require "filepicker_rails/configuration"
 require "filepicker_rails/policy"
 require "filepicker_rails/engine"
 require "filepicker_rails/tag"
+require_relative "../app/helpers/filepicker_rails/application_helper"
+require_relative "../app/helpers/filepicker_rails/form_helper"
 
 module FilepickerRails
 end
